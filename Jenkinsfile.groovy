b89d9d30f42068b2de6a4033d82fd2b696106d94
Mozaic33 <44155625+Mozaic33@users.noreply.github.com>
2019-01-28 15:28:16 +0200

    Add files via upload

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,31 @@
+/ import javaposse.jobdsl.plugin.GlobalJobDslSecurityConfiguration
+import jenkins.model.GlobalConfiguration
+
+node {
+        label 'proiect'
+        ws("/var/lib/jenkins/jobs/nexus/MyWorkspace"){
+	
+	    stage( 'clone repo') {
+		  
+			    git url: "https://github.com/Mozaic33/test.git"
+		    
+	    }
+		stage('Clean') {
+		
+			    sh "mvn clean"
+			
+		}
+		// mvn integration-test
+		stage('Package') {
+		       sh "mvn package"
+		}
+		stage('Start Tomcat')
+		{
+		      sh "sudo cp /var/lib/jenkins/jobs/nexus/MyWorkspace/target/AMGrads.war /usr/share/tomcat/webapps && sudo service tomcat restart"
+		}
+		stage('Test')
+		{
+		      sh "mvn integration-test"
+		}
+		}
+		}
